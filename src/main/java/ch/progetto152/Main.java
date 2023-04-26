a5f49e43f134ba6ca80e3c5a20d882aae20b76fe
Joe Küng <joekueng05@gmail.com>
2023-04-26 10:57:37 +0200

    adding Spring application start

--- src/main/java/ch/progetto152/Main.java
+++ src/main/java/ch/progetto152/Main.java
@@ -1,7 +1,18 @@
 package ch.progetto152;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(Main.class, args);
     }
+
+    @Bean
+    public Controller controller() {
+        return new Controller();
+    }
+
 }
