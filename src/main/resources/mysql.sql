a3f276fbdd19be885ccbbd4002618f52c72453fc
Joe Küng <joekueng05@gmail.com>
2023-04-27 11:04:23 +0200

    db username unique

--- src/main/resources/mysql.sql
+++ src/main/resources/mysql.sql
@@ -30,7 +30,7 @@ create table if not exists User
 (
     id       int          not null auto_increment,
     name     varchar(45)  not null,
-    username varchar(100) not null,
+    username varchar(100) not null unique,
     password varchar(100) not null,
     primary key (id)
 );
