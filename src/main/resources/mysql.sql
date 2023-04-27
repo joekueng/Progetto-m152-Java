09ffbfc3be954eee4e7c3d7d3ec8ba63cbc01a1b
grata <aramis.grata@bluewin.ch>
2023-04-27 10:00:52 +0200

    Added users in sql

--- src/main/resources/mysql.sql
+++ src/main/resources/mysql.sql
@@ -68,3 +68,7 @@ insert into Waypoints (name, lat, lon, description, img, LocationName) values ('
                                                                                'Descrizione del punto 3, un grandissimo',
                                                                                'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png',
                                                                                'Lugano');
+
+insert into User (name, username, password) values ('Mario Rossi', 'mario.rossi', 'password');
+insert into User (name, username, password) values ('Luca Bianchi', 'luca.bianchi', 'password');
+insert into User (name, username, password) values ('Giovanni Verdi', 'giovanni.verdi', 'password');
