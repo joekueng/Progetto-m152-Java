e502accf31065f8aa346232f523c66c01963047f
Joe Küng <joekueng05@gmail.com>
2023-05-08 13:16:16 +0200

    clean up

--- src/main/java/ch/progetto152/repository/WaypointRepository.java
+++ src/main/java/ch/progetto152/repository/WaypointRepository.java
@@ -2,14 +2,11 @@ package ch.progetto152.repository;
 
 import ch.progetto152.entity.WaypointsEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface WaypointRepository extends JpaRepository<WaypointsEntity, Long> {
-
-    Optional<WaypointsEntity> findWaypointByName(String name);
 
     Optional<List<WaypointsEntity>> findAllByLocationName(String locationName);
 
